d8c041ac796c25a9a773c76864628716530e4d2b
msaakaash <msaakaash@hotmail.com>
2025-02-12 00:46:21 +0530

    User Authentication Module updation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PasswordRecovery from "./pages/PasswordRecovery";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />   {/* Login is the default page */}
-       
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/PasswordRecovery" element={<PasswordRecovery />} />
       </Routes>
     </Router>
   );
